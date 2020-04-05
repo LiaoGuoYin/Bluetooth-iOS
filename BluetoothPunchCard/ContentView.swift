8b30a7bd26ed96346d796a16fb0f049b5ff4d6e8
LiaoGuoYin <liaoguoyin@live.com>
2020-04-05 21:54:37 +0800

    Initial Commit

---
+++ BluetoothPunchCard/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BluetoothPunchCard
+//
+//  Created by LiaoGuoYin on 2020/4/5.
+//  Copyright © 2020 LiaoGuoYin. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
