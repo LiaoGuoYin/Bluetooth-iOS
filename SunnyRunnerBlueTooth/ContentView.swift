57aa9e6a9c8489c3bb5cf9180417df198c65019d
LiaoGuoYin <liaoguoyin@live.com>
2020-05-14 08:22:50 +0800

    Initial Commit

---
+++ SunnyRunnerBlueTooth/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SunnyRunnerBlueTooth
+//
+//  Created by LiaoGuoYin on 2020/5/14.
+//  Copyright © 2020 LiaoGuoYin. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
